f7e9c73834a448f155a851eb87447d42508aa839
PeeHaa <info@pieterhordijk.com>
2014-01-18 14:54:56 +0100

    Added bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This bootstraps the library
+ *
+ * PHP version 5.3
+ *
+ * @category   CkanConsumer
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace CkanConsumer;
+
+use CkanConsumer\Common\Autoloader;
+
+require_once __DIR__ . '/src/CkanConsumer/Common/Autoloader.php';
+
+$autoloader = new Autoloader(__NAMESPACE__, dirname(__DIR__));
+$autoloader->register();
